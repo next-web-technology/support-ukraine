173a764d9351ad2225267d2314c1e1bce727d2d4
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-02-26 11:26:15 +0900

    feat: add firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyA2yFhKmKJsv5v-Pa87Ur4kG8PuSPZsO7g',
+    authDomain: 'support-ukraine-nwt.firebaseapp.com',
+    projectId: 'support-ukraine-nwt',
+    storageBucket: 'support-ukraine-nwt.appspot.com',
+    messagingSenderId: '613550690421',
+    appId: '1:613550690421:web:8e7449c167e6d85da2e83c',
+    measurementId: 'G-B7K9530SWL',
+  },
   useEmulator: false,
 };
