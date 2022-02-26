c302d3dba4571dbfd327a2d206359135258a7344
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-02-26 14:26:12 +0900

    feat: add legal currencies accounts info

---
+++ src/app/models/leagl-currencies/leagal-currency.model.ts
@@ -0,0 +1,14 @@
+export type LegalCurrency = {
+  flagIconClass: string;
+  currencyName: string;
+  beneficiary: string;
+  beneficiaryBic: string;
+  beneficiaryAddress: string;
+  accountNumber: string;
+  iban: string;
+  sortCode: string;
+  beneficiaryBankName: string;
+  beneficiaryBankBic: string;
+  beneficiaryBankAddress: string;
+  purposeOfPayment: string;
+};
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  LEGAL_CURRENCIES,
+  LEGAL_CURRENCY_LABELS,
+} from 'src/app/models/leagl-currencies/leagal-currencies';
+import { LegalCurrency } from 'src/app/models/leagl-currencies/leagal-currency.model';
 
 @Component({
   selector: 'app-view-home',
@@ -6,5 +11,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class ViewHomeComponent {
+  legalCurrencyLabels: string[] = LEGAL_CURRENCY_LABELS;
+  legalCurrencies: LegalCurrency[] = LEGAL_CURRENCIES;
+
   constructor() {}
 }
